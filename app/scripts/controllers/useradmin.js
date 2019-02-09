33f4e67fca221c8a7e431d8433adddb1ac07ff1e
slyfox <slyfox2046@gmail.com>
2019-02-09 17:23:49 +0800

    demo2

--- app/scripts/controllers/useradmin.js
+++ app/scripts/controllers/useradmin.js
@@ -1,5 +1,5 @@
 angular.module('ngdemoApp')
-  .controller('userAdminCtrl', function ($scope,$location) {
+  .controller('userAdminCtrl', function ($scope,$location,$state) {
     $('#userTab a').click(function (e) {
       e.preventDefault();
       $(this).tab('show');
@@ -12,6 +12,11 @@ angular.module('ngdemoApp')
     $scope.changeTabName = function (name) {
       $scope.userTab.name = name;
 
+    };
+
+    $scope.goUrl= function (url) {
+      $state.go(url);
+      $scope.changeTabName(url.split('.')[1]);
     };
 
   });
